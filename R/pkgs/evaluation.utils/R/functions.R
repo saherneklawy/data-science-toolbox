f94bb40ef4d1c4fa53d93e1d8ea3bcf591028a7d
Saher El-Neklawy <saher.neklawy@gmail.com>
2015-09-15 16:00:57 +0200

    Updating split data function

--- R/pkgs/evaluation.utils/R/functions.R
+++ R/pkgs/evaluation.utils/R/functions.R
@@ -16,9 +16,31 @@ run_on_sample <- function(data_frame,
                           func,
                           sample_ratio = 0.6,
                           seed = 12239) {
-  set.seed(seed)
   if(is.null(evaluation.utils.data_frame.sample)) {
-    evaluation.utils.data_frame.sample <<- sample(data_frame, as.int(nrow(data_frame)*sample))
+    evaluation.utils.data_frame.sample <<- split_data(data_frame, sample_ratio, seed)
   }
   func(evaluation.utils.data_frame.sample)
+}
+
+
+
+#' A function that splits the data to training and test
+#'
+#' A dummy function to test package creation
+#' @param data_frame
+#' @param training_ratio the sample to take out of the data
+#' @param seed the seed for the sample
+#' @keywords  partitioning data
+#' @export
+#' @examples
+#' split_data(data_frame)$train
+#' split_data(data_frame)$test
+split_data <- function(data_frame, training_ratio = 0.6, seed = 12239) {
+  set.seed(seed)
+  N = nrow(data_frame)
+  data_frame$original_index = 1:N
+  indexes = sample(N, as.integer(N*training_ratio))
+  train = data_frame[ indexes, ]
+  test = data_frame[ -indexes, ]
+  list(train = train, test = test)
 }
