223667d0e51f2b516f3ff1a07a8558cd81b6b09c
Saher El-Neklawy <saher.neklawy@gmail.com>
2015-09-16 10:59:07 +0200

    Adding widescreen function

--- R/pkgs/r.utils/R/functions.R
+++ R/pkgs/r.utils/R/functions.R
@@ -1,6 +1,5 @@
 #' A function that simplifies installing packages
 #'
-#' A dummy function to test package creation
 #' @param pacakge name
 #' @param github repo name and user if installed using github directly
 #' @keywords tools
@@ -18,4 +17,16 @@ install.if.needed <- function(pkg, github = NA) {
       devtools::install_github(github)
     }
   }
+}
+
+
+#' A function that auto adjusts the r terminal columns width
+#' from: http://stackoverflow.com/questions/1172485/how-to-increase-the-number-of-columns-using-r-in-linux
+#'
+#' @keywords tools
+#' @export
+#' @examples
+#' wideScreen()
+wideScreen <- function(howWide=as.numeric(strsplit(system('stty size', intern=T), ' ')[[1]])[2]) {
+   options(width=as.integer(howWide))
 }
